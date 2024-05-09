46043c54a3262f5319792ce71f3c42048d809960
Mr <alvinmasila55@gmail.com>
2024-05-09 15:47:35 +0300

    Your commit message here

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelector('section');
-let navlinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
   sections.forEach(sec => {
@@ -10,17 +10,16 @@ window.onscroll = () => {
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
-    if(top >= offset && top < offset + height){
-      navlinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('header nav a [href*=' + id + ']').class.List.add ('active')
-      })
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
     }
-    
-  })
-}
+  });
+};
 
-menuIcon.oneclick = () => {
-menuIcon.classList.toggle('bx-x');
-navbar.classList.toggle('active');
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
